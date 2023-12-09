8f97c86165a8e68ab03ba33961df733089180b82
Roland Körtvely <roland.kortvely@gmail.com>
2023-12-09 09:37:28 +0100

    Update enpass-to-nordpass.py

--- enpass-to-nordpass.py
+++ enpass-to-nordpass.py
@@ -13,6 +13,29 @@ def is_valid_url_or_domain(url):
         r'([/\w \.-]*)*$', re.IGNORECASE)  # path (optional)
     return re.match(regex, url) is not None
 
+def get_latest_password(field):
+
+    # use field "value" if not empty
+    if field.get('value', ''):
+        return field.get('value', '')
+
+    # Extract the most recent password from the history if available
+    history = field.get('history', [])
+    if history:
+        return history[-1].get('value', '')
+    return field.get('value', '')
+
+def fill_other_fields(field, nordpass_entry):
+    # if field type is username fill username
+    if not nordpass_entry['username'] and field.get('type', '').lower() == 'username':
+        nordpass_entry['username'] = field.get('value', '')
+    # if field type is password fill password
+    elif not nordpass_entry['password'] and field.get('type', '').lower() == 'password':
+        nordpass_entry['password'] = get_latest_password(field)
+    # if field type is email fill username
+    elif not nordpass_entry['username'] and field.get('type', '').lower() == 'email':
+        nordpass_entry['username'] = field.get('value', '')
+
 def convert_enpass_to_nordpass(enpass_file, nordpass_file):
     # Check if Enpass file exists
     if not os.path.exists(enpass_file):
@@ -42,14 +65,15 @@ def convert_enpass_to_nordpass(enpass_file, nordpass_file):
         }
 
         # Mapping Enpass fields to NordPass
+     # Mapping Enpass fields to NordPass
         for field in item.get('fields', []):
             label = field.get('label', '').lower()
             value = field.get('value', '')
-            if label in ['username', 'používateľské meno']:
+            if label in ['používateľské meno', 'username']:
                 nordpass_entry['username'] = value
-            elif label in ['password', 'prihlasovacie heslo']:
-                nordpass_entry['password'] = value
-            elif label in ['url', 'webová stránka']:
+            elif label in ['heslo', 'password']:
+                nordpass_entry['password'] = get_latest_password(field)
+            elif label in ['webová stránka', 'url']:
                 nordpass_entry['url'] = value if value else (title if is_valid_url_or_domain(title) else '')
             elif label in ['držiteľ karty', 'cardholder name']:
                 nordpass_entry['cardholdername'] = value
@@ -59,7 +83,10 @@ def convert_enpass_to_nordpass(enpass_file, nordpass_file):
                 nordpass_entry['cvc'] = value
             elif label in ['dátum platnosti', 'expiry date', 'expirydate']:
                 nordpass_entry['expirydate'] = value
+            else:
+                fill_other_fields(field, nordpass_entry)
 
+       # Ensure URL is set if empty
         if not nordpass_entry['url']:
             nordpass_entry['url'] = title if is_valid_url_or_domain(title) else ''
 
